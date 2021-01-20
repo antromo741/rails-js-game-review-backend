6f42720f2be4bf4c904119fef031bf1576a65027
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-01-20 15:00:47 -0500

    added to the current user controller, made a change in the sessions controller, got ride of the old current user and put before actions into the game and review controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,6 @@
 class ApplicationController < ActionController::API
     rescue_from ActiveRecord::RecordNotFound, with: :handle_record_not_found
-    def current_user
-        User.first
-    end
-
+  
     private 
     def handle_record_not_found
       render json: "Record not found", status: :not_found
